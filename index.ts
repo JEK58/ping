687cf27a7e910f810e1a55b0128de1e8f855eed0
Stephan Schöpe <socialcrap+github@posteo.de>
2023-09-24 19:36:23 +0200

    log added

--- index.ts
+++ index.ts
@@ -3,5 +3,8 @@ import { schedule } from "node-cron";
 const URL = Bun.env.MONITOR_URL;
 
 schedule("*/10 * * * *", async () => {
-  if (URL) await fetch(URL);
+  if (URL) {
+    await fetch(URL);
+    console.log(`Ping sent at ${new Date().toISOString()}`);
+  }
 });
